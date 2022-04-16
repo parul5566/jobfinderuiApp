a3e5efa1521c57fd39295616a9a8c4a47ac80262
ParulSharma <parent0077@gmail.com>
2022-04-16 12:59:12 +0530

    git add

---
+++ lib/main.dart
@@ -0,0 +1,231 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedcoverwidget1/GeneratedCoverWidget1.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedloginwidget/GeneratedLoginWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedlogowidget/GeneratedLogoWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedsplashscreenwidget/GeneratedSplashScreenWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedsignupwidget/GeneratedSignUpWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedforgotpasswordwidget2/GeneratedForgotPasswordWidget2.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedsuccessfullywidget/GeneratedSuccessfullyWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedcheckyouremailwidget/GeneratedCheckYourEmailWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedhomescreenwidget/GeneratedHomeScreenWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generateddescriptionwidget/GeneratedDescriptionWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generateddescriptionwidget1/GeneratedDescriptionWidget1.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedcompanywidget/GeneratedCompanyWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedcompanywidget3/GeneratedCompanyWidget3.dart';
+import 'package:flutterapp/jobfinderuiappapp/generateduploadcvwidget/GeneratedUploadCVWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generateduploadcvwidget2/GeneratedUploadCVWidget2.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedsuccessfulwidget/GeneratedSuccessfulWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedsearchwidget/GeneratedSearchWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatednoresultsfoundwidget/GeneratedNoresultsfoundWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedfilterwidget1/GeneratedFilterWidget1.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedfilterwidget7/GeneratedFilterWidget7.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedspecializationwidget8/GeneratedSpecializationWidget8.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedmyconnectionwidget/GeneratedMyconnectionWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedpostingwidget2/GeneratedPostingWidget2.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedpostwidget/GeneratedPostWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedjobswidget1/GeneratedJobsWidget1.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedaboutuswidget2/GeneratedAboutusWidget2.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedcreatjoborpostwidget/GeneratedCreatJoborPostWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedaddpostwidget/GeneratedAddPostWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedsharedajobwidget/GeneratedSharedaJobWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedaddajobwidget/GeneratedAddajobWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatededitaddajobwidget/GeneratedEditAddajobWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedtypeofworkplacewidget5/GeneratedTypeofworkplaceWidget5.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedchoosejobtypewidget/GeneratedChooseJobTypeWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedjobpositionwidget/GeneratedJobPositionWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedlocationwidget4/GeneratedLocationWidget4.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedcompanywidget17/GeneratedCompanyWidget17.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatednomessagewidget/GeneratedNoMessageWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedmessagewidget/GeneratedMessageWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedchatwidget7/GeneratedChatWidget7.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedoptionswidget16/GeneratedOptionsWidget16.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedsavejobwidget1/GeneratedSaveJobWidget1.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatednosavingswidget2/GeneratedNoSavingsWidget2.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatednotificationswidget/GeneratedNotificationsWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatednotificationswidget2/GeneratedNotificationsWidget2.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedyourapplicationwidget/GeneratedYourapplicationWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedoptionswidget37/GeneratedOptionsWidget37.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatednotificationswidget6/GeneratedNotificationsWidget6.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatednonotificationswidget/GeneratedNonotificationsWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedaboutmewidget1/GeneratedAboutmeWidget1.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedundochangeswidget/GeneratedUndoChangesWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedprofilewidget/GeneratedProfileWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatededitprofilewidget1/GeneratedEditProfileWidget1.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedaddworkexperiencewidget2/GeneratedAddworkexperienceWidget2.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedchangeworkexperiencewidget/GeneratedChangeworkexperienceWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedundochangeswidget3/GeneratedUndoChangesWidget3.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedremoveworkexperiencewidget/GeneratedRemoveWorkExperienceWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedaddeducationwidget/GeneratedAddEducationWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedchangeeducationwidget/GeneratedChangeEducationWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedstartdatewidget/GeneratedStartDateWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedenddatewidget2/GeneratedEndDateWidget2.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedundochangeswidget7/GeneratedUndoChangesWidget7.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedremoveeducationwidget/GeneratedRemoveEducationWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedlevelofeducationwidget/GeneratedLevelofEducationWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedinstitutionnamewidget6/GeneratedInstitutionnameWidget6.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedfieldofstudywidget6/GeneratedFieldofstudyWidget6.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedaddskillwidget/GeneratedAddSkillWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedskill8widget/GeneratedSkill8Widget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedaddskillwidget3/GeneratedAddSkillWidget3.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedsearchlanguagewidget/GeneratedSearchlanguageWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedlanguagewidget5/GeneratedLanguageWidget5.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedremovelanguagewidget/GeneratedRemoveLanguageWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedaddlanguagewidget4/GeneratedAddLanguageWidget4.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedoralwidget2/GeneratedOralWidget2.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedaddappreciationwidget/GeneratedAddAppreciationWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatededitappreciationwidget/GeneratedEditAppreciationWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedundochangeswidget12/GeneratedUndoChangesWidget12.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedremoveappreciationwidget/GeneratedRemoveAppreciationWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedaddresumewidget/GeneratedAddResumeWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generateduploadresumewidget/GeneratedUploadresumeWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedmyprofilev1widget/GeneratedMyProfileV1Widget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedsettingswidget/GeneratedSettingsWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedlogoutwidget2/GeneratedLogoutWidget2.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedupdatepasswordwidget/GeneratedUpdatePasswordWidget.dart';
+import 'package:flutterapp/jobfinderuiappapp/generatedaddresumev2widget/GeneratedAddResumeV2Widget.dart';
+
+void main() {
+  runApp(jobFinderUiAppApp());
+}
+
+class jobFinderUiAppApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedCoverWidget1',
+      routes: {
+        '/GeneratedCoverWidget1': (context) => GeneratedCoverWidget1(),
+        '/GeneratedLoginWidget': (context) => GeneratedLoginWidget(),
+        '/GeneratedLogoWidget': (context) => GeneratedLogoWidget(),
+        '/GeneratedSplashScreenWidget': (context) =>
+            GeneratedSplashScreenWidget(),
+        '/GeneratedSignUpWidget': (context) => GeneratedSignUpWidget(),
+        '/GeneratedForgotPasswordWidget2': (context) =>
+            GeneratedForgotPasswordWidget2(),
+        '/GeneratedSuccessfullyWidget': (context) =>
+            GeneratedSuccessfullyWidget(),
+        '/GeneratedCheckYourEmailWidget': (context) =>
+            GeneratedCheckYourEmailWidget(),
+        '/GeneratedHomeScreenWidget': (context) => GeneratedHomeScreenWidget(),
+        '/GeneratedDescriptionWidget': (context) =>
+            GeneratedDescriptionWidget(),
+        '/GeneratedDescriptionWidget1': (context) =>
+            GeneratedDescriptionWidget1(),
+        '/GeneratedCompanyWidget': (context) => GeneratedCompanyWidget(),
+        '/GeneratedCompanyWidget3': (context) => GeneratedCompanyWidget3(),
+        '/GeneratedUploadCVWidget': (context) => GeneratedUploadCVWidget(),
+        '/GeneratedUploadCVWidget2': (context) => GeneratedUploadCVWidget2(),
+        '/GeneratedSuccessfulWidget': (context) => GeneratedSuccessfulWidget(),
+        '/GeneratedSearchWidget': (context) => GeneratedSearchWidget(),
+        '/GeneratedNoresultsfoundWidget': (context) =>
+            GeneratedNoresultsfoundWidget(),
+        '/GeneratedFilterWidget1': (context) => GeneratedFilterWidget1(),
+        '/GeneratedFilterWidget7': (context) => GeneratedFilterWidget7(),
+        '/GeneratedSpecializationWidget8': (context) =>
+            GeneratedSpecializationWidget8(),
+        '/GeneratedMyconnectionWidget': (context) =>
+            GeneratedMyconnectionWidget(),
+        '/GeneratedPostingWidget2': (context) => GeneratedPostingWidget2(),
+        '/GeneratedPostWidget': (context) => GeneratedPostWidget(),
+        '/GeneratedJobsWidget1': (context) => GeneratedJobsWidget1(),
+        '/GeneratedAboutusWidget2': (context) => GeneratedAboutusWidget2(),
+        '/GeneratedCreatJoborPostWidget': (context) =>
+            GeneratedCreatJoborPostWidget(),
+        '/GeneratedAddPostWidget': (context) => GeneratedAddPostWidget(),
+        '/GeneratedSharedaJobWidget': (context) => GeneratedSharedaJobWidget(),
+        '/GeneratedAddajobWidget': (context) => GeneratedAddajobWidget(),
+        '/GeneratedEditAddajobWidget': (context) =>
+            GeneratedEditAddajobWidget(),
+        '/GeneratedTypeofworkplaceWidget5': (context) =>
+            GeneratedTypeofworkplaceWidget5(),
+        '/GeneratedChooseJobTypeWidget': (context) =>
+            GeneratedChooseJobTypeWidget(),
+        '/GeneratedJobPositionWidget': (context) =>
+            GeneratedJobPositionWidget(),
+        '/GeneratedLocationWidget4': (context) => GeneratedLocationWidget4(),
+        '/GeneratedCompanyWidget17': (context) => GeneratedCompanyWidget17(),
+        '/GeneratedNoMessageWidget': (context) => GeneratedNoMessageWidget(),
+        '/GeneratedMessageWidget': (context) => GeneratedMessageWidget(),
+        '/GeneratedChatWidget7': (context) => GeneratedChatWidget7(),
+        '/GeneratedOptionsWidget16': (context) => GeneratedOptionsWidget16(),
+        '/GeneratedSaveJobWidget1': (context) => GeneratedSaveJobWidget1(),
+        '/GeneratedNoSavingsWidget2': (context) => GeneratedNoSavingsWidget2(),
+        '/GeneratedNotificationsWidget': (context) =>
+            GeneratedNotificationsWidget(),
+        '/GeneratedNotificationsWidget2': (context) =>
+            GeneratedNotificationsWidget2(),
+        '/GeneratedYourapplicationWidget': (context) =>
+            GeneratedYourapplicationWidget(),
+        '/GeneratedOptionsWidget37': (context) => GeneratedOptionsWidget37(),
+        '/GeneratedNotificationsWidget6': (context) =>
+            GeneratedNotificationsWidget6(),
+        '/GeneratedNonotificationsWidget': (context) =>
+            GeneratedNonotificationsWidget(),
+        '/GeneratedAboutmeWidget1': (context) => GeneratedAboutmeWidget1(),
+        '/GeneratedUndoChangesWidget': (context) =>
+            GeneratedUndoChangesWidget(),
+        '/GeneratedProfileWidget': (context) => GeneratedProfileWidget(),
+        '/GeneratedEditProfileWidget1': (context) =>
+            GeneratedEditProfileWidget1(),
+        '/GeneratedAddworkexperienceWidget2': (context) =>
+            GeneratedAddworkexperienceWidget2(),
+        '/GeneratedChangeworkexperienceWidget': (context) =>
+            GeneratedChangeworkexperienceWidget(),
+        '/GeneratedUndoChangesWidget3': (context) =>
+            GeneratedUndoChangesWidget3(),
+        '/GeneratedRemoveWorkExperienceWidget': (context) =>
+            GeneratedRemoveWorkExperienceWidget(),
+        '/GeneratedAddEducationWidget': (context) =>
+            GeneratedAddEducationWidget(),
+        '/GeneratedChangeEducationWidget': (context) =>
+            GeneratedChangeEducationWidget(),
+        '/GeneratedStartDateWidget': (context) => GeneratedStartDateWidget(),
+        '/GeneratedEndDateWidget2': (context) => GeneratedEndDateWidget2(),
+        '/GeneratedUndoChangesWidget7': (context) =>
+            GeneratedUndoChangesWidget7(),
+        '/GeneratedRemoveEducationWidget': (context) =>
+            GeneratedRemoveEducationWidget(),
+        '/GeneratedLevelofEducationWidget': (context) =>
+            GeneratedLevelofEducationWidget(),
+        '/GeneratedInstitutionnameWidget6': (context) =>
+            GeneratedInstitutionnameWidget6(),
+        '/GeneratedFieldofstudyWidget6': (context) =>
+            GeneratedFieldofstudyWidget6(),
+        '/GeneratedAddSkillWidget': (context) => GeneratedAddSkillWidget(),
+        '/GeneratedSkill8Widget': (context) => GeneratedSkill8Widget(),
+        '/GeneratedAddSkillWidget3': (context) => GeneratedAddSkillWidget3(),
+        '/GeneratedSearchlanguageWidget': (context) =>
+            GeneratedSearchlanguageWidget(),
+        '/GeneratedLanguageWidget5': (context) => GeneratedLanguageWidget5(),
+        '/GeneratedRemoveLanguageWidget': (context) =>
+            GeneratedRemoveLanguageWidget(),
+        '/GeneratedAddLanguageWidget4': (context) =>
+            GeneratedAddLanguageWidget4(),
+        '/GeneratedOralWidget2': (context) => GeneratedOralWidget2(),
+        '/GeneratedAddAppreciationWidget': (context) =>
+            GeneratedAddAppreciationWidget(),
+        '/GeneratedEditAppreciationWidget': (context) =>
+            GeneratedEditAppreciationWidget(),
+        '/GeneratedUndoChangesWidget12': (context) =>
+            GeneratedUndoChangesWidget12(),
+        '/GeneratedRemoveAppreciationWidget': (context) =>
+            GeneratedRemoveAppreciationWidget(),
+        '/GeneratedAddResumeWidget': (context) => GeneratedAddResumeWidget(),
+        '/GeneratedUploadresumeWidget': (context) =>
+            GeneratedUploadresumeWidget(),
+        '/GeneratedMyProfileV1Widget': (context) =>
+            GeneratedMyProfileV1Widget(),
+        '/GeneratedSettingsWidget': (context) => GeneratedSettingsWidget(),
+        '/GeneratedLogoutWidget2': (context) => GeneratedLogoutWidget2(),
+        '/GeneratedUpdatePasswordWidget': (context) =>
+            GeneratedUpdatePasswordWidget(),
+        '/GeneratedAddResumeV2Widget': (context) =>
+            GeneratedAddResumeV2Widget(),
+      },
+    );
+  }
+}
